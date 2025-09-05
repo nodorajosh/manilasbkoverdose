51b73bd5900f142db32c0e0cf57ccdd79b7ab144
nodorajosh <joshuaruedennodora@gmail.com>
2025-09-05 17:18:13 +0400

    Implement code changes to enhance functionality and improve performance

--- src/app/home/artists.components/artist-section.component.tsx
+++ src/app/home/artists.components/artist-section.component.tsx
@@ -1,53 +1,40 @@
 import Image from "next/image";
 
-export default function ArtistsSectionComponent({
-    title,
-    blurb,
-    image,
-}: {
-    title: string;
-    blurb: string;
-    image: string[];
-}) {
+
+// const images = filenames.map(name => path.join('/', dirRelativeToPublicFolder, name))
+
+// console.log(filenames)
+
+export default function ArtistsSectionComponent({ title, blurb, image }: { title: string, blurb: string, image: string[] }) {
+
     return (
         <div className="relative flex flex-row flex-wrap">
-            {/* Left text section */}
             <div className="px-6 pt-12 pb-2 lg:pb-12 lg:min-h-[56dvh] w-full lg:w-[33%] bg-(--color-neutral-dark) grid place-items-center z-5">
                 <div className="glow px-6 py-6 shadow flex flex-col flex-wrap justify-center content-center bg-gradient-to-tr from-secondary-dark via-neutral-dark via-black via-neutral-dark to-secondary-dark rounded">
                     <h1 className="text-2xl text-center">
-                        <span>&nbsp;{title}&nbsp;</span>
+                        <span className="">&nbsp;{title}&nbsp;</span>
                     </h1>
-                    <p className="pt-4 max-w-[85ch]">{blurb}</p>
+                    <p className="pt-4 max-w-[85ch]">
+                        {blurb}
+                    </p>
                 </div>
             </div>
-
-            {/* Right image section */}
             <div className="relative w-full lg:w-[67%] grid place-items-center">
-                <div className="image-section max-w-full min-h-[40dvh] lg:min-h-[56dvh] flex flex-wrap justify-center items-center">
-                    {/* Left spacer */}
-                    <div className="min-w-[9dvh] lg:min-w-[13dvh]">&nbsp;</div>
 
-                    {/* If images exist, show them, otherwise fallback message */}
-                    {image && image.length > 0 ? (
-                        image.map((img: string, k: number) => (
-                            <Image
-                                key={k}
-                                src={img}
-                                alt="artist image"
-                                width={600}
-                                height={600}
-                                className="image mx-2 my-6 h-[32dvh] lg:h-[48dvh] w-auto rounded"
-                            />
-                        ))
-                    ) : (
-                        <p className="text-center text-lg font-medium text-white opacity-80">
-                            Artists Coming Soon...
-                        </p>
-                    )}
-
-                    {/* Right spacer */}
-                    <div className="min-w-[9dvh] lg:min-w-[13dvh]">&nbsp;</div>
-                </div>
+                {image && image.length > 0 ? (
+                    <div className="image-section max-w-full min-h-[40dvh] lg:min-h-[56dvh]">
+                        <div className="min-w-[9dvh] lg:min-w-[13dvh]">&nbsp;</div>
+                        {image?.map((image: string, k: number) => (
+                            <Image key={k} src={image} alt="image" width={600} height={600} className="image mx-2 my-6 h-[32dvh] lg:h-[48dvh] w-auto rounded" />
+                        ))}
+                        <div className="min-w-[9dvh] lg:min-w-[13dvh]">&nbsp;</div>
+                    </div>) : (
+                    <div className="image-section max-w-full min-h-[40dvh] lg:min-h-[56dvh] flex-wrap content-center justify-center">
+                        <div className="min-w-[9dvh] lg:min-w-[13dvh]">&nbsp;</div>
+                        <p className="text-center text-lg italic opacity-50">Artists to be announced soon. Stay tuned!</p>
+                        <div className="min-w-[9dvh] lg:min-w-[13dvh]">&nbsp;</div>
+                    </div>
+                )}
             </div>
         </div>
     );
